2a200240fb5e851e096fb6dce57e7116d0ac6ea9
trsno <17230640+trsno@users.noreply.github.com>
2023-12-02 11:01:19 +0000

    part2 of day 1

--- 2023/Day1/trebuchet.ts
+++ 2023/Day1/trebuchet.ts
@@ -9,18 +9,68 @@ async function readFile(filePath: string): Promise<string> {
 	}
 }
 
-function calculateSum(data: string): number {
-	return data.split('\n').reduce((acc, str) => {
-		const digits = str.match(/\d(?=\D*$)|\d/g) ?? [];
-		return acc + (digits.length > 0 ? parseInt(`${digits[0]}${digits?.pop()}`, 10) : 0);
-	}, 0);
+function calculateSum(data: string): { [key: string]: number } {
+	return data.split('\n').reduce(
+		(acc, str, index) => {
+			const partOneDigits = str.match(/\d(?=\D*$)|\d/g) ?? [];
+			const partOneDigitA = partOneDigits[0] ?? 0;
+			const partOneDigitB = partOneDigits?.pop() ?? 0;
+			acc['part1'] += partOneDigits.length > 0 ? parseInt(`${partOneDigitA}${partOneDigitB}`, 10) : 0;
+
+			const digitMap: { [key: string]: string } = {
+				zero: '0',
+				one: '1',
+				two: '2',
+				three: '3',
+				four: '4',
+				five: '5',
+				six: '6',
+				seven: '7',
+				eight: '8',
+				nine: '9'
+			};
+
+			const partTwoDigitA = str
+				.toLowerCase()
+				.match(new RegExp(`\(?:${Object.keys(digitMap).join('|')}\|\\d)`))![0]
+				.replace(new RegExp(`\(?:${Object.keys(digitMap).join('|')})`, 'i'), (match) => digitMap[match] ?? match);
+
+			const partTwoDigitB = str
+				.split('')
+				.reverse()
+				.join('')
+				.toLowerCase()
+				.match(
+					new RegExp(
+						`\(?:${Object.keys(digitMap)
+							.map((str) => str.split('').reverse().join(''))
+							.join('|')}\|\\d)`
+					)
+				)![0]
+				.replace(
+					new RegExp(
+						`\(?:${Object.keys(digitMap)
+							.map((str) => str.split('').reverse().join(''))
+							.join('|')})`,
+						'i'
+					),
+					(match) => digitMap[match.split('').reverse().join('')] ?? match.split('').reverse().join('')
+				);
+
+			acc['part2'] += parseInt(`${partTwoDigitA}${partTwoDigitB}`, 10) ?? 0;
+
+			console.log(`${index + 1}.	[${str}]\n	part1: [${+partOneDigitA}${+partOneDigitB}]\n	part2: [${+partTwoDigitA}${+partTwoDigitB}]\n`);
+			return acc;
+		},
+		{ part1: 0, part2: 0 }
+	);
 }
 
 async function main() {
 	try {
 		const strings = await readFile('./input.txt');
 		const result = calculateSum(strings);
-		console.log(result);
+		console.log(`\nTotal\npart1: ${result['part1']}\npart2: ${result['part2']}`);
 	} catch (error) {
 		console.error(error);
 	}
