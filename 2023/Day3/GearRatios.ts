5d843aad129fcf56ac7bd852b658d63d5fef54bc
trsno <17230640+trsno@users.noreply.github.com>
2023-12-03 14:47:04 +0000

    day3

---
+++ 2023/Day3/GearRatios.ts
@@ -0,0 +1,60 @@
+import { readFile as fsReadFile } from 'fs/promises';
+import { resolve } from 'path';
+import { performance } from 'perf_hooks';
+
+const readFile = async (filePath: string): Promise<string> =>
+	await fsReadFile(resolve(__dirname, filePath), 'utf8').catch((error) => {
+		throw `Failed: ${error instanceof Error ? error.message : error}`;
+	});
+
+const isNumberInRange = (number: number, min: number, max: number) => number >= min && number <= max;
+
+const getResults = (input: string[]): { part1: number; part2: number } => {
+	const results = { part1: 0, part2: 0 };
+	const numbers = input.flatMap((item, y) => {
+		let indexOffset = 0;
+		const match = item.match(/\d+/g);
+		return !match
+			? []
+			: match.map((num) => {
+					const index = item.indexOf(num, indexOffset);
+					indexOffset = index + num.length;
+					return { x: index, y, num: +num, used: false };
+			  });
+	});
+
+	input.forEach((item, y) => {
+		[...item].forEach((char, x) => {
+			const charList: number[] = [];
+			if (/[^\d.]/.test(char)) {
+				numbers.filter((v) => {
+					const valid = isNumberInRange(x, v.x - 1, v.x + String(v.num).length) && isNumberInRange(v.y, y - 1, y + 1) && !v.used;
+					valid && (v.used = true);
+					char == '*' && valid && charList.push(v.num);
+					return valid;
+				});
+			}
+			if (charList.length == 2) {
+				results.part2 += charList[0] * charList[1];
+			}
+		});
+	});
+
+	results.part1 = numbers.reduce((sum, obj) => (obj.used ? sum + obj.num : sum), 0);
+	return results;
+};
+
+async function main() {
+	try {
+		const startTime = performance.now();
+		const input = (await readFile('./input.txt')).split('\n');
+		const results = getResults(input);
+
+		const time = performance.now() - startTime;
+		console.log(`Part 1: ${results.part1}\nPart 2: ${results.part2}\nTimer: ${time} ms`);
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+main().catch((error) => console.error(error));
